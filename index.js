649f729020920ab84af0d3787690ea748d5c3165
yingwerica <yingw.pro@gmail.com>
2022-11-03 16:46:53 -0600

    get single member

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require ('express');
+const { parse } = require('path');
+const path = require('path');
+const members = require('./members');
+
+const logger = require('./middleware/logger.js');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-});
+
+
+//init middleware
+// app.use(logger);
+
+//gets all members
+app.get('/api/members', (req, res) => res.json(members));
+
+//get single member
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+    // res.send(req.params.id);
+    if(found) {
+      res.json(members.filter(member => member.id === parseInt(req.params.id)));  
+    } else {
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`})
+    }
+    
+})
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 const PORT = process.env.PORT || 5000;
 
