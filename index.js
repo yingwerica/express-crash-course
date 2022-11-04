ae4954cb22ae8ce31481be447eef3e0084ea3458
yingwerica <yingw.pro@gmail.com>
2022-11-03 21:39:22 -0600

    render template

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require ('express');
 const path = require('path');
-
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members')
 
 const app = express();
-
-
 
 //init middleware
 // app.use(logger);
 
+//handlebar Middleware
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Homepage Route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
+}));
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Body parser Middleware
 app.use(express.json());
@@ -20,8 +31,7 @@ app.use(express.urlencoded({extended: false }));
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
 
-//set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 //Members API Routes
 app.use('/api/members', require('./routes/api/members'))
