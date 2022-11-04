63a470ed709021a5ddef8a653810d4bb8b97561f
yingwerica <yingw.pro@gmail.com>
2022-11-03 20:34:53 -0600

    all the CRUD finished

--- routes/api/members.js
+++ routes/api/members.js
@@ -30,10 +30,45 @@ router.post('/', (req, res) => {
     if(!newMember.name || !newMember.email) {
         return res.status(400).json({ msg: 'Please include a name and email'});
     }
-
+    
     members.push(newMember);
     res.json(members);
 
+});
+
+//update Member
+router.put('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+    
+    if(found) {
+        const updMember = req.body;
+        members.forEach(member => {
+            if(member.id ===parseInt(req.params.id)) {
+                member.name = updMember.name ? updMember.name : member.name;
+                member.email = updMember.email ? updMember.email : member.email;
+                
+                res.json({ msg: 'Member updated', member});
+            }
+        })  
+    } else {
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`})
+    }
+    
+});
+
+
+//delete Member
+router.delete('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+    
+    if(found) {
+      res.json({ 
+        msg: 'Member deleted',
+        members: members.filter(member => member.id !== parseInt(req.params.id))});  
+    } else {
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`})
+    }
+    
 });
 
 module.exports = router;
